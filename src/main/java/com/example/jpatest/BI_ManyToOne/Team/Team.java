340ba8279678ee2eb71dc96c84a9c188ec316ee4
mintuchel <max3627@naver.com>
2024-09-10 23:37:53 +0900

    update: comments

--- src/main/java/com/example/jpatest/BI_ManyToOne/Team/Team.java
+++ src/main/java/com/example/jpatest/BI_ManyToOne/Team/Team.java
@@ -18,7 +18,7 @@ public class Team {
 
     // mappedBy 에는 주인 엔티티 변수명. DB 칼럼명이 아님!!
     // FetchType.LAZY : 실제로 참조하기 전에는 프록시 객체로 구성
-    // CascadeType.ALL : 부모인 Team entity에 영속성 변화가 일어나면 Member에도 영속성 전이를 함
+    // CascadeType.ALL (PERSIST + REMOVE) : 부모인 Team entity에 영속성 변화가 일어나면 Member에도 영속성 전이를 함
     // orphanRemoval : 부모인 Team과의 관계가 끊어져 Member가 고아가 되면 Member는 자동으로 삭제됨
     @OneToMany(mappedBy="team", fetch=FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Member> members = new ArrayList<>();
